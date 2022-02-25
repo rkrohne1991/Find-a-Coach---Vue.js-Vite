b1329f6f9ec56194ce17631afe230ddb1dbd0cf7
Rafal <rafal.krohne@hotmail.co.uk>
2022-02-25 15:03:21 +0100

    Outputting Incoming Requests (Messages)

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,1 +1,8 @@
-export const getters = {};
+export const getters = {
+  requests(state) {
+    return state.requests;
+  },
+  hasRequests(state) {
+    return state.requests && state.requests.length > 0;
+  },
+};
