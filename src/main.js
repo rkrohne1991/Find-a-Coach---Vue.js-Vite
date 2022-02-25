d800f4925ece5ca6607a60983016780b09fa661a
Rafal <rafal.krohne@hotmail.co.uk>
2022-02-25 08:48:54 +0100

    Adding a Couple of Base Components (Base Card, Base Button, Base Badge)

--- src/main.js
+++ src/main.js
@@ -5,9 +5,17 @@ import store from "./store/index.js";
 
 import App from "./App.vue";
 
+import BaseBadge from "./components/ui/BaseBadge.vue";
+import BaseButton from "./components/ui/BaseButton.vue";
+import BaseCard from "./components/ui/BaseCard.vue";
+
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component("base-badge", BaseBadge);
+app.component("base-button", BaseButton);
+app.component("base-card", BaseCard);
 
 app.mount("#app");
